e2755691299d69ae6ebe0b75406461a78c7042df
jfilian1 <jamesfilian@jfilian1.local>
2015-04-13 09:25:52 -0500

    Initial Commit

--- EventsApp/AppDelegate.swift
+++ EventsApp/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.setApplicationId("sGpMIcNUqxLu3TPurTe174M3fO1Xfbqo8P5D8oIJ", clientKey: "J51dWazZ1vaXMHy6Ltm9g7jHTD9YJoHZDsjmqIC2")
         return true
     }
 
